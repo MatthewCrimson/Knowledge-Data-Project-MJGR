1af2d953cd119ccf537332fea3e36306d14042af
Matthijs <matthijs.vogel@Live.nl>
2019-10-24 18:50:32 +0200

    final patch

--- js/main.js
+++ js/main.js
@@ -5,53 +5,46 @@ function mainCtrl($scope, $http, ChartJsProvider){
   $scope.runQuery = function(){
     $scope.queries = [];
     $scope.locations = [];
+
+    //SPARQL queries
     if (document.getElementById("Restaurants").checked) {
       console.log("Adding Restaurants");
       $scope.queries.push("PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>PREFIX fp: <http://www.semanticweb.org/group69/finalproject/>select Distinct * where { ?context rdf:type fp:Restaurants .?context fp:hasLatitude ?lat .?context fp:hasLongitude ?lng .}")
-    }
-    if (document.getElementById("Fastfood").checked) {
+    } if (document.getElementById("Fastfood").checked) {
       console.log("Adding Fastfood");
       $scope.queries.push("PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX fp: <http://www.semanticweb.org/group69/finalproject/> select Distinct * where { ?context rdf:type fp:FastFoodRestaurant .?context fp:hasLatitude ?lat .?context fp:hasLongitude ?lng .}");
-    }
-    if (document.getElementById("FrenchRestaurants").checked) {
+    } if (document.getElementById("FrenchRestaurants").checked) {
       console.log("Adding French Restaurants");
       $scope.queries.push("PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX fp: <http://www.semanticweb.org/group69/finalproject/> select Distinct * where { ?context rdf:type fp:FrenchRestaurant .?context fp:hasLatitude ?lat .?context fp:hasLongitude ?lng .}");
-    }
-    if (document.getElementById("Bowling").checked) {
+    } if (document.getElementById("Bowling").checked) {
       console.log("Adding Bowling");
       $scope.queries.push("PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX fp: <http://www.semanticweb.org/group69/finalproject/> select Distinct * where { ?context rdf:type fp:Bowling .?context fp:hasLatitude ?lat .?context fp:hasLongitude ?lng .}");
-    }
-    if (document.getElementById("Gyms").checked) {
+    } if (document.getElementById("Gyms").checked) {
       console.log("Adding Gyms");
       $scope.queries.push("PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX fp: <http://www.semanticweb.org/group69/finalproject/> select Distinct * where { ?context rdf:type fp:Gyms .?context fp:hasLatitude ?lat .?context fp:hasLongitude ?lng .}");
-    }
-    if (document.getElementById("Musea").checked) {
+    } if (document.getElementById("Musea").checked) {
       console.log("Adding Musea");
       $scope.queries.push("PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX fp: <http://www.semanticweb.org/group69/finalproject/> select Distinct * where { ?context rdf:type fp:Musea .?context fp:hasLatitude ?lat .?context fp:hasLongitude ?lng .}");
-    }
-    if (document.getElementById("Art").checked) {
+    } if (document.getElementById("Art").checked) {
       console.log("Adding Art");
       $scope.queries.push("PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX fp: <http://www.semanticweb.org/group69/finalproject/> select Distinct * where { ?context rdf:type fp:ArtMusea .?context fp:hasLatitude ?lat .?context fp:hasLongitude ?lng .}");
-    }
-    if (document.getElementById("History").checked) {
+    } if (document.getElementById("History").checked) {
       console.log("Adding History");
       $scope.queries.push("PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX fp: <http://www.semanticweb.org/group69/finalproject/> select Distinct * where { ?context rdf:type fp:HistoryMusea .?context fp:hasLatitude ?lat .?context fp:hasLongitude ?lng .}");
-    }
-    if (document.getElementById("EscapeRoom").checked) {
+    } if (document.getElementById("EscapeRoom").checked) {
       console.log("Adding EscapeRoom");
       $scope.queries.push("PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX fp: <http://www.semanticweb.org/group69/finalproject/> select Distinct * where { ?context rdf:type fp:EscapeRooms .?context fp:hasLatitude ?lat .?context fp:hasLongitude ?lng .}");
-    }
-    if (document.getElementById("Minigolf").checked) {
+    } if (document.getElementById("Minigolf").checked) {
       console.log("Adding Minigolf");
       $scope.queries.push("PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX fp: <http://www.semanticweb.org/group69/finalproject/> select Distinct * where { ?context rdf:type fp:MiniGolf .?context fp:hasLatitude ?lat .?context fp:hasLongitude ?lng .}");
-    }
-    if (document.getElementById("Shopping").checked) {
+    } if (document.getElementById("Shopping").checked) {
       console.log("Adding Shopping");
       $scope.queries.push("PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX fp: <http://www.semanticweb.org/group69/finalproject/> select Distinct * where { ?context rdf:type fp:Shopping .?context fp:hasLatitude ?lat .?context fp:hasLongitude ?lng .}");
     }
 
     var counter = 0;
 
+    //Run all queries
     for (var query in $scope.queries) {
       $http( {
         method: "GET",
@@ -77,6 +70,7 @@ function mainCtrl($scope, $http, ChartJsProvider){
           console.log("counter: " + counter);
           console.log("queries: " + $scope.queries.length);
 
+          //Check whether all queries have been completed
           if (counter == $scope.queries.length) {
             console.log("ready, result:");
             console.log($scope.locations);
@@ -84,6 +78,7 @@ function mainCtrl($scope, $http, ChartJsProvider){
             var infowindow = new google.maps.InfoWindow();
             var marker, i;
 
+            //Remove existing markers
             for (var i = 0; i < marker_list.length; i++) {
               marker_list[i].setMap(null);
             }
@@ -91,6 +86,7 @@ function mainCtrl($scope, $http, ChartJsProvider){
             
             bounds = new google.maps.LatLngBounds();
 
+            //Add new markers
             for (i = 0; i < $scope.locations.length; i++) {  
               marker = new google.maps.Marker({
                 position: new google.maps.LatLng($scope.locations[i][0], $scope.locations[i][1]),
@@ -99,7 +95,8 @@ function mainCtrl($scope, $http, ChartJsProvider){
 
               loc = new google.maps.LatLng(marker.position.lat(), marker.position.lng());
               bounds.extend(loc);
-        
+              
+              //Add infowindow to markers
               google.maps.event.addListener(marker, 'click', (function(marker, i) {
                 return function() {
                   infowindow.setContent($scope.locations[i][2]);
@@ -110,10 +107,10 @@ function mainCtrl($scope, $http, ChartJsProvider){
             }
             console.log(marker_list);
             
+            //Set limit to autozoom
             google.maps.event.addListener(map, 'zoom_changed', function() {
               zoomChangeBoundsListener = google.maps.event.addListener(map, 'bounds_changed', function(event) {
                       if (this.getZoom() > 15 && this.initialZoom == true) {
-                          // Change max/min zoom here
                           this.setZoom(15);
                           this.initialZoom = false;
                       }
@@ -122,9 +119,7 @@ function mainCtrl($scope, $http, ChartJsProvider){
             });
             
             map.initialZoom = true;
-            map.fitBounds(bounds);
-            // map.panToBounds(bounds);
-            
+            map.fitBounds(bounds);            
           }
         })
         .error(function(error ){
